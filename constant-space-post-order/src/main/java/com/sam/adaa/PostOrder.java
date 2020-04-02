e2abf85569f20c11989c65fdf92643710f6f8f9d
SamratK <samrat.2k16@gmail.com>
2020-04-02 22:50:04 +0530

    Updated post order class.

--- constant-space-post-order/src/main/java/com/sam/adaa/PostOrder.java
+++ constant-space-post-order/src/main/java/com/sam/adaa/PostOrder.java
@@ -35,42 +35,6 @@ public class PostOrder {
 		TreeVisualizer.createTree(tree.root);
 	}
 
-	public static void main2(String[] args) {
-		PostOrder tree = new PostOrder();
-		tree.root = new TreeNode(1);
-		tree.root.left = new TreeNode(2);
-		tree.root.right = new TreeNode(3);
-		tree.root.left.left = new TreeNode(4);
-		tree.root.left.left.left = new TreeNode(8);
-		tree.root.left.left.right = new TreeNode(9);
-		tree.root.left.left.right.left = new TreeNode(12);
-		tree.root.left.right = new TreeNode(5);
-		tree.root.right.left = new TreeNode(6);
-		tree.root.right.right = new TreeNode(7);
-		tree.root.right.right.left = new TreeNode(10);
-		tree.root.right.right.right = new TreeNode(11);
-		tree.root.right.right.left.left = new TreeNode(13);
-		tree.root.right.right.right.right = new TreeNode(14);
-		tree.root.right.right.left.left.right = new TreeNode(15);
-		tree.root.right.right.left.left.right.left = new TreeNode(16);
-
-		System.out.println("Post order traversal :-");
-		tree.postOrder(tree.root);
-		System.out.println();
-		System.out.println();
-		System.out.println("Constant space post order traversal method 1 :-");
-		tree.postOrderTraversalConstantSpace1(tree.root);
-		System.out.println();
-		System.out.println();
-		System.out.println("Constant space post order traversal method 2 :-");
-		tree.postOrderTraversalConstantSpace2(tree.root);
-		System.out.println();
-		System.out.println();
-		System.out.println("Constant space post order traversal method 3 :-");
-		tree.postOrderTraversalConstantSpace3(tree.root);
-		TreeVisualizer.createTree(tree.root);
-	}
-
 	/**
 	 * Using pointers to predecessors and dummy root node.
 	 * 
